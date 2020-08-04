3e382373f37ca26f5970063e111a83c1f6fa11a2
dangermyong <66460964+dangermyong@users.noreply.github.com>
2020-08-04 19:34:04 +0900

    modify search method

--- routes/search.js
+++ routes/search.js
@@ -4,16 +4,22 @@ const router = express.Router();
 const pool = require('../utils/mysql.js');
 
 router.get('/pro', async (req, res, next) => {
-  try{
-    const connection = await pool.getConnection();
-    const [results] = await connection.query('SELECT * FROM GOSU_TB');
-    connection.release();
-    res.render('searchPro', {data : results});
-  } catch (err) {
-    console.log(err);
-    res.json({ status : 500, msg : '에러가 났어요!'});
-  }
+  searchOptions = req.query.search;
+  if ( searchOptions == null ) { searchOptions = "" }
+    try{
+      const connection = await pool.getConnection();
+      const [results] = await connection.query('SELECT * FROM GOSU_TB WHERE CONCAT(subject, address) LIKE "%"?"%" OR CONCAT(subject, address) LIKE "%"?"%"',[searchOptions, searchOptions]);
+      connection.release();
+      res.render('searchPro', {data : results});
+    } catch (err) {
+      console.log(err);
+      res.json({ status : 500, msg : '에러가 났어요!'});
+    }
   
-});
+  // let sql;
+  // if ( searchOptions != null && searchOptions !=='') {
+  //   sql = 'SELECT * FROM GOSU_TB WHERE name = ?';
+  // } else { sql = 'SELECT * FROM GOSU_TB'}
+  });
 
 module.exports = router;
