02a42aa34ef484cac0edecb1bf7c537ead38b397
dangermyong <66460964+dangermyong@users.noreply.github.com>
2020-08-05 17:13:00 +0900

    two words search

--- routes/search.js
+++ routes/search.js
@@ -4,22 +4,30 @@ const router = express.Router();
 const pool = require('../utils/mysql.js');
 
 router.get('/pro', async (req, res, next) => {
-  searchOptions = req.query.search;
-  if ( searchOptions == null ) { searchOptions = "" }
+  search = req.query.search.replace(" ","|");
+  if ( search == null ) { search = "" }
     try{
       const connection = await pool.getConnection();
-      const [results] = await connection.query('SELECT * FROM GOSU_TB WHERE CONCAT(subject, address) LIKE "%"?"%" OR CONCAT(subject, address) LIKE "%"?"%"',[searchOptions, searchOptions]);
+      const [results] = await connection.query('SELECT * FROM GOSU_TB WHERE CONCAT(subject, address) REGEXP ?', search);
       connection.release();
       res.render('searchPro', {data : results});
     } catch (err) {
       console.log(err);
       res.json({ status : 500, msg : '에러가 났어요!'});
     }
-  
-  // let sql;
-  // if ( searchOptions != null && searchOptions !=='') {
-  //   sql = 'SELECT * FROM GOSU_TB WHERE name = ?';
-  // } else { sql = 'SELECT * FROM GOSU_TB'}
   });
+// router.get('/pro', async (req, res, next) => {
+//   searchOptions = req.query.search;
+//   if ( searchOptions == null ) { searchOptions = "" }
+//     try{
+//       const connection = await pool.getConnection();
+//       const [results] = await connection.query('SELECT * FROM GOSU_TB WHERE CONCAT(subject, address) LIKE "%"?"%" OR CONCAT(subject, address) LIKE "%"?"%"',[searchOptions, searchOptions]);
+//       connection.release();
+//       res.render('searchPro', {data : results});
+//     } catch (err) {
+//       console.log(err);
+//       res.json({ status : 500, msg : '에러가 났어요!'});
+//     }
+//   });
 
 module.exports = router;
