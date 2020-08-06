59109de9775aea6341ff809690091f0541fdd6f6
dangermyong <66460964+dangermyong@users.noreply.github.com>
2020-08-06 12:19:59 +0900

    search bar complete

--- routes/search.js
+++ routes/search.js
@@ -1,33 +1,30 @@
 const express = require("express");
 const router = express.Router();
-
 const pool = require('../utils/mysql.js');
 
 router.get('/pro', async (req, res, next) => {
-  search = req.query.search.replace(" ","|");
-  if ( search == null ) { search = "" }
-    try{
-      const connection = await pool.getConnection();
-      const [results] = await connection.query('SELECT * FROM GOSU_TB WHERE CONCAT(subject, address) REGEXP ?', search);
-      connection.release();
-      res.render('searchPro', {data : results});
-    } catch (err) {
-      console.log(err);
-      res.json({ status : 500, msg : '에러가 났어요!'});
-    }
-  });
-// router.get('/pro', async (req, res, next) => {
-//   searchOptions = req.query.search;
-//   if ( searchOptions == null ) { searchOptions = "" }
-//     try{
-//       const connection = await pool.getConnection();
-//       const [results] = await connection.query('SELECT * FROM GOSU_TB WHERE CONCAT(subject, address) LIKE "%"?"%" OR CONCAT(subject, address) LIKE "%"?"%"',[searchOptions, searchOptions]);
-//       connection.release();
-//       res.render('searchPro', {data : results});
-//     } catch (err) {
-//       console.log(err);
-//       res.json({ status : 500, msg : '에러가 났어요!'});
-//     }
-//   });
+  let searchOptions = req.query.search;
+  let searchQuery ="hi";
+  if (typeof searchOptions == "undefined") {
+    searchQuery = "SELECT * FROM GOSU_TB"
+  } else if (searchOptions.indexOf(' ') >= 0) {
+    searchOptions = searchOptions.replace(" ","|");
+    searchQuery = `SELECT * FROM GOSU_TB WHERE CONCAT(subject, address) REGEXP "${searchOptions}"`;
+  } else {
+    searchQuery = `SELECT * FROM GOSU_TB WHERE CONCAT(subject, address) REGEXP "${searchOptions}"`;
+
+  }
+  console.log(searchQuery);
+  try{
+    const connection = await pool.getConnection();
+    
+    const [results] = await connection.query(searchQuery);
+    connection.release();
+    res.render('searchPro', {data : results});
+  } catch (err) {
+    console.log(err);
+    res.json({ status : 500, msg : '에러가 났어요!'});
+  }
+});
 
 module.exports = router;
