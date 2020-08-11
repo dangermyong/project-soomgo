5e783d825ba04d18ee89c57ed2a89aa5c7b0d8a4
dangermyong <66460964+dangermyong@users.noreply.github.com>
2020-08-11 09:12:08 +0900

    resolve merge conflict

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 
@@ -22,13 +22,5 @@ router.get('/app-download', function(req, res, next) {
 router.get('/estimates', function(req, res, next) {
   res.render('estimates.ejs');
 });
-
-router.get('/sign-up', function(req, res, next){
-  res.render('sign-up.ejs')
-})
-
-router.get('/sign-up/user', function(req, res, next){
-  res.render('user-signup.ejs')
-})
 
 module.exports = router;
--- routes/search.js
+++ routes/search.js
@@ -1,33 +1,34 @@
 const express = require("express");
 const router = express.Router();
-
 const pool = require('../utils/mysql.js');
 
 router.get('/pro', async (req, res, next) => {
-  search = req.query.search.replace(" ","|");
-  if ( search == null ) { search = "" }
-    try{
-      const connection = await pool.getConnection();
-      const [results] = await connection.query('SELECT * FROM GOSU_TB WHERE CONCAT(subject, address) REGEXP ?', search);
-      connection.release();
-      res.render('searchPro', {data : results});
-    } catch (err) {
-      console.log(err);
-      res.json({ status : 500, msg : '에러가 났어요!'});
+  let searchOptions = req.query.search;
+  let searchQuery ="hi";
+  if (typeof searchOptions == "undefined") {
+    searchQuery = "SELECT * FROM GOSU_TB"
+  } else if (searchOptions.indexOf(' ') >= 0) {
+    searchOptions = searchOptions.replace(" ","|");
+    searchQuery = `SELECT * FROM GOSU_TB WHERE CONCAT(subject, address) REGEXP "${searchOptions}"`;
+  } else {
+    searchQuery = `SELECT * FROM GOSU_TB WHERE CONCAT(subject, address) REGEXP "${searchOptions}"`;
+  }
+  try{
+    const connection = await pool.getConnection();
+    const [results] = await connection.query(searchQuery);
+    for (result of results) {
+      console.log(result.id);
+      const [review] = await connection.query(`SELECT * FROM REVIEW_TB WHERE gosu_id = ${result.id}`);
+      result.review_name = review[0].user_name;
+      result.review_comment = review[0].comment;
+      console.log(result);
     }
-  });
-// router.get('/pro', async (req, res, next) => {
-//   searchOptions = req.query.search;
-//   if ( searchOptions == null ) { searchOptions = "" }
-//     try{
-//       const connection = await pool.getConnection();
-//       const [results] = await connection.query('SELECT * FROM GOSU_TB WHERE CONCAT(subject, address) LIKE "%"?"%" OR CONCAT(subject, address) LIKE "%"?"%"',[searchOptions, searchOptions]);
-//       connection.release();
-//       res.render('searchPro', {data : results});
-//     } catch (err) {
-//       console.log(err);
-//       res.json({ status : 500, msg : '에러가 났어요!'});
-//     }
-//   });
+    connection.release();
+    res.render('searchPro', {data : results});
+  } catch (err) {
+    console.log(err);
+    res.json({ status : 500, msg : '에러가 났어요!'});
+  }
+});
 
 module.exports = router;
